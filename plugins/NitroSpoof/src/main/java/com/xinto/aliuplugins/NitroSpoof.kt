e50e661f2ffc6cd635367fa9e8f01f1be6da7e9c
akieAkachan <162443858+akieAkachan@users.noreply.github.com>
2024-04-29 01:55:35 +0300

    Update NitroSpoof.kt

--- plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/NitroSpoof.kt
+++ plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/NitroSpoof.kt
@@ -67,7 +67,7 @@ class NitroSpoof : Plugin() {
         finalUrl += idStr
         val emoteSize = settings.getString(EMOTE_SIZE_KEY, EMOTE_SIZE_DEFAULT).toIntOrNull()
 
-        finalUrl += (if (isAnimated) ".gif" else ".png") + "?quality=lossless&name=" + emoteName
+        finalUrl += (if (isAnimated) ".gif" else ".png")
 
         if (emoteSize != null) {
             finalUrl += "&size=${emoteSize}"
